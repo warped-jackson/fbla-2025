edc54aaffc496bbfc0aa3292c821c3e0758d8eef
Jackson <jackson.d.cabral@gmail.com>
2025-01-12 13:26:18 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+from variables import *
+
+
+
+# Window Maker
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Fix It Later")
+clock = pygame.time.Clock()
+
+
+# Game Loop
+running = True
+while running:
+    #event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            running = False
+
+    clock.tick(fps)
+    screen.fill(black)
+    pygame.display.flip()
+
+
+
