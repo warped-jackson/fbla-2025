81c0eeb48acf7d45062672087754725098bb8182
Jackson <jackson.d.cabral@gmail.com>
2025-01-18 13:44:44 -0500

    Refactoring

--- main.py
+++ main.py
@@ -2,27 +2,56 @@ import pygame
 import random
 from settings import *
 
+class Game:
+    def __init(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Fix It Later")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def new(self):
+        all_sprites = pygame.sprite.group()
+
+def run(self):
+    self.playing = True
+    while self.playing:
+        self.clock.tick(fps)
+        self.update()
+        self.events()
+        self.draw()
 
 
-# Window Maker
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption("Fix It Later")
-clock = pygame.time.Clock()
+def update(self):
+    pass
 
-
-# Game Loop
-running = True
-while running:
-    #event
+def events(self):
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
+        if event.type == pygame.QUIT:
+            if self.playing:
+                self.playing = False
             running = False
 
-    clock.tick(fps)
+
+def draw(self):
     screen.fill(red)
+    all_sprites.draw(screen)
     pygame.display.flip()
+
+
+def show_go_screen(self):
+    pass
+
+g = Game()
+#g.show_start_screen
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pygame.quit()
+# Window Maker
+#pygame.display.flip()
 
 
 
